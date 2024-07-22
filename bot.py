a022f5f1558327aeffaf0452e826811d58c7e143
Something231 <119870385+Something231@users.noreply.github.com>
2024-07-22 18:03:16 +0800

    adding command class

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
+import discord
 from discord import Client, Intents, app_commands
+from discord.ext import commands
 import matplotlib as plot
 import asyncio
 
+class CommandsManager(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @app_commands.AppCommand()
+    async def ping(interaction: discord.Interaction):
+        return await interaction.response.send_message("e")
 
 class ActivityBot:
     """
@@ -15,5 +24,7 @@ class ActivityBot:
             Send Messages
             Read Message History
         """
+        #self.bot = #
         self.intents = Intents(68608)
-        
+
+        await 
